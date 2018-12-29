2bc4cf7447859d64312aa2b5c9c29472c1c9a782
NKBelousov <belousovnikita92@gmail.com>
2018-12-29 13:35:38 +0300

    ignore dist

--- dist/handlers/IndexHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const IndexHandler = (req, res) => {
-    res.json({
-        message: `Hello, Express`,
-    });
-};
-exports.default = IndexHandler;
-//# sourceMappingURL=IndexHandler.js.map
