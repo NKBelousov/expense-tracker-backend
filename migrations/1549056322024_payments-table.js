51cdcdeb32f23d3f1773652b738d04881fd99356
NKBelousov <belousovnikita92@gmail.com>
2019-02-02 00:32:57 +0300

    create users_payments table

---
+++ migrations/1549056322024_payments-table.js
@@ -0,0 +1,39 @@
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable("users_payments", {
+    user_id: {
+      notNull: true,
+      references: "users(id)",
+      type: "id",
+      unique: false,
+    },
+    type_id: {
+      notNull: true,
+      references: "payment_types(id)",
+      type: "id",
+      unique: false,
+    },
+    name: {
+      notNull: true,
+      unique: false,
+      type: "varchar(100)",
+    },
+    cost: {
+      notNull: true,
+      unique: false,
+      type: "float",
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+exports.down = pgm => {
+  pgm.dropTable("users_payments", {
+    ifExists: true,
+  });
+};
