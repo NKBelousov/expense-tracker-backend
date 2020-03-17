3a63a2682057b24b247e7ff0042378188d15204e
NKBelousov <belousovnikita92@gmail.com>
2020-03-17 21:20:48 +0300

    fix authorization header not being allowed

--- src/middlewares/AccessControlMiddleware.ts
+++ src/middlewares/AccessControlMiddleware.ts
@@ -1,9 +1,20 @@
 import { RequestHandler, Request, Response, NextFunction } from "express";
 
-const handler: RequestHandler = function (req: Request, res: Response, next: NextFunction) {
+const handler: RequestHandler = function(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method.toUpperCase() === "OPTIONS") {
+    res.status(200).send();
+    return;
+  }
   next();
-}
+};
 
 export default handler;
